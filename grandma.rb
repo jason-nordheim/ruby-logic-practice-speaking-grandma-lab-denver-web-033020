0a4d083ceaf08a2dcc77fed9972b71c59faa97f1
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-06 22:51:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,7 @@ def speak_to_grandma statement
   if statement == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif statements == 'WHAT DID YOU EAT TODAY?' || 'WHAT?'
+    "NO, NOT SINCE 1938!"
   else
     'HUH?! SPEAK UP, SONNY!'
   end
