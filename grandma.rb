493d545b736fa5d7c29ed45946349138591888ba
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-06 22:49:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-  
+def speak_to_grandma statement
+  if statement = 'I LOVE YOU GRANDMA!'
+    
+  end
 end
 
 # Whatever you say to grandma, she should respond with
